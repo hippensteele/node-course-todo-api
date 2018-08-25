5548c53aa88e98304db2d76cfe9b22312c64376a
Tom <hippensteele@gmail.com>
2018-08-25 21:48:01 +0000

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -18,7 +19,8 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc); 
     },(e) => {
-        res.status(400).send(e); 
+        res.status(400).send(); 
+        console.log(e);
     });
 });
 
@@ -26,7 +28,24 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos}); 
     },(e) => {
-        res.status(400).send(e); 
+        res.status(400).send(); 
+        console.log(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    };
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            return res.status(404).send();
+        }
+        res.send({todo}); 
+    },(e) => {
+        res.status(400).send(); 
+        console.log(e);
     });
 });
 
